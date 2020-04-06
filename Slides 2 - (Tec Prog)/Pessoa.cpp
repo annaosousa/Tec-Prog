17f5b01f4e6d781da8e43f690dd3cce38ff1635d
annaosousa <58178338+annaosousa@users.noreply.github.com>
2020-04-06 15:10:38 -0300

    Add files via upload

---
+++ Slides 2 - (Tec Prog)/Pessoa.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "Pessoa.h"
+#include "Universidade.h"
+#include "Principal.h"
+#include <string.h>
+
+Pessoa::Pessoa(int diaNa, int mesNa, int anoNa, char* nome)
+{
+    //inicializa(diaNa, mesNa, anoNa, nome);
+}
+
+Pessoa::Pessoa()
+{
+    //inicializa(0,0,0,"");
+}
+
+void Pessoa::calcula_idade(int diaAt, int mesAt, int anoAt)
+{
+    idadeP = anoAt - anoP;
+        if (mesP > mesAt)
+            idadeP--;
+        else
+            if (mesP == mesAt)
+                if (diaP > diaAt)
+                    idadeP--;
+    std::cout <<"Nome e idade, sao reespectivamente, " << nomeP << " e " << idadeP << std::endl;
+}
+
+int Pessoa::informaIdade()
+{
+    return idadeP;
+}
+
+void Pessoa::inicializa(int diaNa, int mesNa, int anoNa, char* nome)
+{
+    diaP = diaNa;
+    mesP = mesNa;
+    anoP = anoNa;
+    idadeP = -1;
+    strcpy(nomeP,nome);
+
+}
+
+void Pessoa::setUniversidadeFiliado(Universidade* pu)
+{
+    pUniversidadeFiliado = pu;
+}
+
+void Pessoa::OndeTrabalha()
+{
+    std::cout << nomeP << " trabalha para " << pUniversidadeFiliado->getNome() << std::endl;
+}
---
+++ Slides 2 - (Tec Prog)/Universidade.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+#include "Universidade.h"
+#include "Principal.h"
+
+using namespace std;
+
+Universidade::Universidade (char* n)
+{
+    strcpy (nome,n);
+}
+
+Universidade::Universidade()
+{
+
+}
+Universidade::~Universidade()
+{
+
+}
+
+void Universidade::setNome(char* n)
+{
+    strcpy (nome,n);
+}
+
+char* Universidade::getNome()
+{
+    return nome;
+}
