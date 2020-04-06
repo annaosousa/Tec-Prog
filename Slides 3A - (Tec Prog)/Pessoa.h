2228e0ce29adb4ae3a8a3e0406674f9542c6a064
annaosousa <58178338+annaosousa@users.noreply.github.com>
2020-04-06 15:11:47 -0300

    Add files via upload

---
+++ Slides 3A - (Tec Prog)/Pessoa.h
@@ -0,0 +1,37 @@
+#ifndef PESSOA_H_INCLUDED
+#define PESSOA_H_INCLUDED
+#include <stdio.h>
+#include "Universidade.h"
+
+class Pessoa
+{
+private:
+    int diaP;
+    int mesP;
+    int anoP;
+    int idadeP;
+    char nomeP[30];
+
+    Universidade* pUniversidadeFiliado; //objeto
+    Departamento* pDepartamentoFiliado;
+
+public:
+    Pessoa(int diaNa, int mesNa, int anoNa, const char* nome = "");
+    Pessoa (); //função construtora, a qual não recebe parametro
+    ~Pessoa(); //destrutora
+
+    void calcula_idade(int diaAt, int mesAt, int anoAt); // função para o calculo da idade com base no ano atual
+    int informaIdade(); //pode ser substituido pelo getIdade();
+    void inicializa(int diaNa, int mesNa, int anoNa, const char* nome);// inicializa variaveis
+
+    void setNome(char* n); // relaciona nome
+    char*getNome(); //imprime nome
+
+    void setUniversidadeFiliado(Universidade* pu); //associa universidade
+    void setDepartamento(Departamento* Dpto); //associa departamento
+    void QualDepartamento(); // imprime o deparatmento assoiado
+    void OndeTrabalha(); //imprime o local assoiado
+};
+
+
+#endif // PESSOA_H_INCLUDED
---
+++ Slides 3A - (Tec Prog)/Principal.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "Principal.h"
+#include "Departamento.h"
+#include "Universidade.h"
+
+using namespace std;
+
+Principal::Principal()
+{
+    Simao.inicializa ( 3, 10, 1976, "Jean Simao");
+    Einstein.inicializa ( 14, 3, 1879, "Albert Einstein");
+    Newton.inicializa ( 4, 1, 1643, "Isaac Newton");
+
+    UTFPR.setNome ("UTFPR");
+    Princeton.setNome ("Universidade de Princeton");
+    Cambridge.setNome ("Universidade de Cambridge");
+
+    Simao.setUniversidadeFiliado(&UTFPR);
+    Einstein.setUniversidadeFiliado(&Princeton);
+    Newton.setUniversidadeFiliado(&Cambridge);
+
+    Simao.setDepartamento(&DAINF);
+    Einstein.setDepartamento(&FisicaPrinceton);
+    Newton.setDepartamento(&MatematicaCambridge);
+
+    DAINF.setNome("Informatica");
+    DAELN.setNome("Eletronica");
+    FisicaPrinceton.setNome("Fisica/Priceton");
+    MatematicaCambridge.setNome("Matematica/Cambridge");
+
+    UTFPR.setNomeDpto(&DAINF,0);
+    UTFPR.setNomeDpto(&DAELN,1);
+    Princeton.setNomeDpto(&FisicaPrinceton,0);
+    Cambridge.setNomeDpto(&MatematicaCambridge,0);
+}
+
+Principal::~Principal()
+{
+
+}
+
+void Principal::executar()
+{
+    Simao.calcula_idade(2,3,2020);
+    Einstein.calcula_idade(2,3,2020);
+    Newton.calcula_idade(2,3,2020);
+
+    Simao.OndeTrabalha();
+    Einstein.OndeTrabalha();
+    Newton.OndeTrabalha();
+
+    Simao.QualDepartamento();
+    Einstein.QualDepartamento();
+    Newton.QualDepartamento();
+
+}
