36039a2d86a52187f3d0f3c24173531e39c55c78
annaosousa <58178338+annaosousa@users.noreply.github.com>
2020-04-06 15:14:22 -0300

    Add files via upload

---
+++ Slides 3C - (Tec Prog) - List/Professor.h
@@ -0,0 +1,23 @@
+#ifndef PROFESSOR_H_INCLUDED
+#define PROFESSOR_H_INCLUDED
+#include "Pessoa.h"
+#include "Universidade.h"
+
+class Professor:public Pessoa
+{
+private:
+    Universidade* pUniversidadeFiliado; //objeto
+    Departamento* pDepartamentoFiliado;
+
+public:
+    Professor(int dia,int mes, int ano, const char* nome = "");
+    Professor();// construtora
+    ~Professor(); //destrutora
+
+    void setUniversidadeFiliado(Universidade* pu); //associa universidade
+    void setDepartamento(Departamento* Dpto); //associa departamento
+    void QualDepartamento();
+    void OndeTrabalha();
+};
+
+#endif // PROFESSOR_H_INCLUDED
