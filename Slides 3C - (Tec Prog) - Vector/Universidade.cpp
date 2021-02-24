104700e86aef4a86f704786600eeafab80b77637
Anna Caroline <58178338+annaosousa@users.noreply.github.com>
2021-02-24 19:14:46 -0300

    Add files via upload

---
+++ Slides 3C - (Tec Prog) - Vector/Universidade.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include "Universidade.h"
+#include "Departamento.h"
+
+using namespace std;
+
+Universidade::Universidade ()
+{
+    int tam = (int)Dpto.size();
+    for(int i = 0; i < tam; i++)
+    {
+        (Dpto[i]) = nullptr;
+    }
+}
+
+Universidade::~Universidade()
+{
+
+}
+
+void Universidade::setNome(const char* n)
+{
+    strcpy (nome,n);
+}
+
+char* Universidade::getNome()
+{
+    return nome;
+}
+
+void Universidade::setNomeDpto(Departamento* nome_dpto)
+{
+    Dpto.push_back(nome_dpto);
+}
+
+void Universidade::imprimeDpto()
+{
+  int tam = (int)Dpto.size();
+  for(int i = 0; i < tam; i++)
+  {
+      std::cout << (Dpto[i])->getNome() << std::endl;
+  }
+}
